a96462db68c121d559074ed1c0bf4f2a523ab9a1
7799043 <leoniak.maciek1@gmail.com>
2024-05-15 13:44:48 +0200

    .

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from expense import Expense
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -22,6 +23,27 @@ def add_expense():
 def save_expense_to_file(expense: Expense, expense_file_path):
     with open(expense_file_path, "a") as f:
         f.write(f"{expense.name},{expense.amount},{expense.category}\n")
+
+# Get nessesary information to calculate budget 
+# @app.route("/show_budget", methods=["GET"])
+
+# def show_budget():
+#     pass
+
+# def summarize_expenses(expense_file_path, budget):
+#     print(f"🎯 Summarizing User Expense")
+#     expenses: list[Expense] = []
+#     with open(expense_file_path, "r") as f:
+#         lines = f.readlines()
+#         for line in lines:
+#             expense_name, expense_amount, expense_category = line.strip().split(",")
+#             line_expense = Expense(
+#                 name=expense_name,
+#                 amount=float(expense_amount),
+#                 category=expense_category,
+#             )
+#             expenses.append(line_expense)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
